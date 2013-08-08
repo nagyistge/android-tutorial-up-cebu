2c4d03599cc3ffcbd41a6ce473448104157b4c50
Rod Michael Coronel <rodmaykel@gmail.com>
2013-08-08 17:00:44 +0800

    activity_1.RegisterActivity.4

--- android/src/com/example/iinstagraam/RegisterActivity.java
+++ android/src/com/example/iinstagraam/RegisterActivity.java
@@ -1,11 +1,16 @@
 package com.example.iinstagraam;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class RegisterActivity extends Activity {
-
+	
+	final static String TAG = "RegisterActivity";
+	Context mContext = this;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,26 @@ public class RegisterActivity extends Activity {
 	    WebView wv = (WebView) findViewById(R.id.wvRegister);
 	    wv.getSettings().setJavaScriptEnabled(true);
 	    wv.loadUrl(getString(R.string.register_url));
+	    
+	    RegisterWebViewClient registerWebViewClient = new RegisterWebViewClient();
+	    wv.setWebViewClient(registerWebViewClient);
 	}
 
+	final class RegisterWebViewClient extends WebViewClient {
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			// TODO Auto-generated method stub
+			Log.d(TAG, url);
+			if (url.equals(mContext.getString(R.string.register_url_success))) {
+				Log.d(TAG, "closing RegisterActivity");
+				finish();
+				return true;
+			} else {
+				return super.shouldOverrideUrlLoading(view, url);
+			}
+		}
+		
+	}
 }
+
