8c56b10e9c3cdc9608b6c3eebcc36e3bf4ec2a39
Rod Michael Coronel <rodmaykel@gmail.com>
2013-08-10 23:42:11 +0800

    activity_3.#2Camera_API

--- android/src/com/example/iinstagraam/PhotoStreamActivity.java
+++ android/src/com/example/iinstagraam/PhotoStreamActivity.java
@@ -1,5 +1,6 @@
 package com.example.iinstagraam;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import org.json.JSONException;
@@ -13,16 +14,25 @@ import com.example.iinstagraam.util.ParserUtil;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PhotoStreamActivity extends Activity {
 
+	protected static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	
 	private static String TAG = "PhotoStreamActivity";
 	private int mPage;
 	
@@ -47,7 +57,43 @@ public class PhotoStreamActivity extends Activity {
 	    mContext = this;
 	    photoList = (ListView) findViewById(R.id.listPhotos);
 	    
+	    ivAddPhoto = (ImageView) findViewById(R.id.ivAddPhoto);
+	    ivAddPhoto.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				
+				File imagesFolder = new File(Environment.getExternalStorageDirectory(), "IinstagraamImages");
+			    imagesFolder.mkdirs(); // <----
+			    long unixTime = System.currentTimeMillis() / 1000L;
+			    File image = new File(imagesFolder, unixTime + ".jpg");
+			    Uri fileUri = Uri.fromFile(image);
+			    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+			    // start the image capture Intent
+			    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+			}
+		});
+	    
 	    new PhotoStreamAsync().execute();
+	}
+	
+	// http://developer.android.com/guide/topics/media/camera.html
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+	        if (resultCode == RESULT_OK) {
+	            // Image captured and saved to fileUri specified in the Intent
+	            Toast.makeText(this, "Image saved to:\n" +
+	                     data.getData(), Toast.LENGTH_LONG).show();
+	            
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // User cancelled the image capture
+	        } else {
+	            // Image capture failed, advise user
+	        }
+	    }
 	}
 
 	private class PhotoStreamAsync extends AsyncTask<Integer, Void, String> {
